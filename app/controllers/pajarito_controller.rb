7ba26dcfb82b3c877fc90699f253604d13aa6d57
Helton Bustos <heltonsmith@hotmail.com>
2022-05-05 10:55:57 -0400

    agregué una vista de detalle a los pajaritos

--- app/controllers/pajarito_controller.rb
+++ app/controllers/pajarito_controller.rb
@@ -10,4 +10,18 @@ class PajaritoController < ApplicationController
         @paginacion = Kaminari.paginate_array(@resultado).page(params[:page]).per(5)
     end
 
+
+    def detalle 
+        @uid = params[:uid]
+        
+        RestClient.get("https://aves.ninjas.cl/api/birds/#{@uid}") { |response, request, result|
+            if response.code ==  200
+                @resultado = JSON.parse(response)
+            else
+                @resultado = "No hay resultados"
+            end
+        }
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   get "/", to: "pajarito#index"
+  get "/detalle/:uid", to: "pajarito#detalle"
 end
